a285699e761cc3e4ef18f0b6fb7ec3981f2dcc34
Vladimir Turov <v.turov@eastbanctech.ru>
2016-04-06 23:53:45 +0600

    Adding service layer

---
+++ PanoramioSDK/GeoArea.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PanoramioSDK
+{
+    public class GeoArea
+    {
+        public GeoArea(double minimalLongitude,
+                       double minimalLatitude,
+                       double maximalLongitude,
+                       double maximalLatitude)
+        {
+            if (minimalLongitude > maximalLongitude)
+                throw new ArgumentException("Wrong longitude range specified: maximal is less than minimal");
+            if (minimalLatitude > maximalLatitude)
+                throw new ArgumentException("Wrong latitude range specified: maximal is less than minimal");
+
+            MinimalLongitude = minimalLongitude;
+            MinimalLatitude = minimalLatitude;
+            MaximalLongitude = maximalLongitude;
+            MaximalLatitude = maximalLatitude;
+        }
+
+        public double MinimalLongitude { get; private set; }
+        public double MinimalLatitude { get; private set; }
+        public double MaximalLongitude { get; private set; }
+        public double MaximalLatitude { get; private set; }
+    }
+}
---
+++ PanoramioSDK/PanoramioClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace PanoramioSDK
+{
+    public class PanoramioClient
+    {
+        private readonly HttpClient httpClient;
+
+        public PanoramioClient()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public async Task<IEnumerable<Photo>> GetPhotosAsync(GeoArea geoArea, PagingInfo paging)
+        {
+            if (geoArea == null) throw new ArgumentNullException(nameof(geoArea));
+            if (paging == null) throw new ArgumentNullException(nameof(paging));
+
+            var uri = ComposeUri(geoArea, paging);
+            var response = await httpClient.GetStringAsync(uri);
+            var jsonObject = JObject.Parse(response);
+            var photosField = jsonObject["photos"];
+            var photos = photosField.ToObject<IEnumerable<Photo>>();
+
+            return photos;
+        }
+
+        private Uri ComposeUri(GeoArea geoArea, PagingInfo paging)
+        {
+            const string baseUri = "http://www.panoramio.com/map/get_panoramas.php?set=public&size=medium&";
+            var parameters = 
+                $"from={paging.From}&to={paging.To}" +
+                $"&minx={geoArea.MinimalLongitude.ToString(CultureInfo.InvariantCulture)}" +
+                $"&miny={geoArea.MinimalLatitude.ToString(CultureInfo.InvariantCulture)}" +
+                $"&maxx={geoArea.MaximalLongitude.ToString(CultureInfo.InvariantCulture)}" +
+                $"&maxy={geoArea.MaximalLatitude.ToString(CultureInfo.InvariantCulture)}";
+
+            return new Uri(baseUri + parameters);
+        }
+    }
+}
